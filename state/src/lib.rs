ac250dd39e49cd2fca29ddfd0c4c7074fb4a2330
none00y <mich_smyk@wp.pl>
2024-03-21 18:11:43 +0100

    fix state

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -1,8 +1,7 @@
 #![no_std]
 
-use escrow_io::*;
+use io::*;
 use gstd::prelude::*;
-use io::{InvariantConfig, InvariantState};
 use gmeta::metawasm;
 
 #[metawasm]
@@ -10,7 +9,7 @@ pub mod metafns {
     pub type State = InvariantState;
 
     pub fn protocol_fee(state: State) -> u128 {
-        let config = *state.config;
+        let config = state.config;
 
         config.protocol_fee
     }
