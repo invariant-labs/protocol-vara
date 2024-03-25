b715320b4c2d15dcd5b920aa4261a52bfc7e0e89
none00y <mich_smyk@wp.pl>
2024-03-25 15:44:50 +0100

    add ticks collection

--- src/contracts/collections/mod.rs
+++ src/contracts/collections/mod.rs
@@ -1,7 +1,9 @@
 pub mod fee_tiers;
 pub mod pool_keys;
 pub mod pools;
+pub mod ticks;
 
 pub use fee_tiers::*;
 pub use pool_keys::*;
 pub use pools::*;
+pub use ticks::*;
---
+++ src/contracts/collections/ticks.rs
@@ -0,0 +1,128 @@
+use crate::{PoolKey, Tick, InvariantError};
+
+use gstd::collections::HashMap;
+
+#[derive(Debug, Default)]
+pub struct Ticks {
+  ticks: HashMap<(PoolKey, i32), Tick>,
+}
+
+impl Ticks {
+  pub fn add(
+      &mut self,
+      pool_key: PoolKey,
+      index: i32,
+      tick: &Tick,
+  ) -> Result<(), InvariantError> {
+      self.ticks
+          .get(&(pool_key, index))
+          .map_or(Ok(()), |_| Err(InvariantError::TickAlreadyExist))?;
+
+      self.ticks.insert((pool_key, index), tick.clone());
+      Ok(())
+  }
+
+  pub fn update(
+      &mut self,
+      pool_key: PoolKey,
+      index: i32,
+      tick: &Tick,
+  ) -> Result<(), InvariantError> {
+      self.get(pool_key, index)?;
+
+      self.ticks.insert((pool_key, index), tick.clone());
+      Ok(())
+  }
+
+  pub fn remove(&mut self, pool_key: PoolKey, index: i32) -> Result<(), InvariantError> {
+      self.get(pool_key, index)?;
+
+      self.ticks.remove(&(pool_key, index));
+      Ok(())
+  }
+
+  pub fn get(&self, pool_key: PoolKey, index: i32) -> Result<Tick, InvariantError> {
+      let tick = self
+          .ticks
+          .get(&(pool_key, index))
+          .cloned()
+          .ok_or(InvariantError::TickNotFound)?;
+
+      Ok(tick)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::FeeTier;
+  use math::percentage::Percentage;
+  use decimal::*;
+  use gstd::ActorId;
+
+  #[test]
+  fn test_add() {
+      let ticks = &mut Ticks::default();
+      let token_x = ActorId::from([0x01; 32]);
+      let token_y = ActorId::from([0x02; 32]);
+      let fee_tier = FeeTier {
+          fee: Percentage::new(0),
+          tick_spacing: 1,
+      };
+      let pool_key = PoolKey::new(token_x, token_y, fee_tier).unwrap();
+      let tick = Tick::default();
+
+      ticks.add(pool_key, 0, &tick).unwrap();
+      assert_eq!(ticks.get(pool_key, 0), Ok(tick));
+      assert_eq!(ticks.get(pool_key, 1), Err(InvariantError::TickNotFound));
+
+      let result = ticks.add(pool_key, 0, &tick);
+      assert_eq!(result, Err(InvariantError::TickAlreadyExist));
+  }
+
+  #[test]
+  fn test_update() {
+      let ticks = &mut Ticks::default();
+      let token_x = ActorId::from([0x01; 32]);
+      let token_y = ActorId::from([0x02; 32]);
+      let fee_tier = FeeTier {
+          fee: Percentage::new(0),
+          tick_spacing: 1,
+      };
+      let pool_key = PoolKey::new(token_x, token_y, fee_tier).unwrap();
+      let tick = Tick::default();
+      let new_tick = Tick {
+          seconds_outside: 1,
+          ..Tick::default()
+      };
+
+      ticks.add(pool_key, 0, &tick).unwrap();
+
+      ticks.update(pool_key, 0, &new_tick).unwrap();
+      assert_eq!(ticks.get(pool_key, 0), Ok(new_tick));
+
+      let result = ticks.update(pool_key, 1, &new_tick);
+      assert_eq!(result, Err(InvariantError::TickNotFound));
+  }
+
+  #[test]
+  fn test_remove() {
+      let ticks = &mut Ticks::default();
+      let token_x = ActorId::from([0x01; 32]);
+      let token_y = ActorId::from([0x02; 32]);
+      let fee_tier = FeeTier {
+          fee: Percentage::new(0),
+          tick_spacing: 1,
+      };
+      let pool_key = PoolKey::new(token_x, token_y, fee_tier).unwrap();
+      let tick = Tick::default();
+
+      ticks.add(pool_key, 0, &tick).unwrap();
+
+      ticks.remove(pool_key, 0).unwrap();
+      assert_eq!(ticks.get(pool_key, 0), Err(InvariantError::TickNotFound));
+
+      let result = ticks.remove(pool_key, 0);
+      assert_eq!(result, Err(InvariantError::TickNotFound));
+  }
+}
