32d80d8e2239e6d52cbfedaf7a297030539f7ef7
none00y <mich_smyk@wp.pl>
2024-03-26 14:56:07 +0100

    change copies to references in pools collection

--- src/contracts/collections/pools.rs
+++ src/contracts/collections/pools.rs
@@ -7,34 +7,34 @@ pub struct Pools {
 }
 
 impl Pools {
-  pub fn add(&mut self, pool_key: PoolKey, pool: &Pool) -> Result<(), InvariantError> {
+  pub fn add(&mut self, pool_key: &PoolKey, pool: &Pool) -> Result<(), InvariantError> {
       self.pools
-          .get(&pool_key)
+          .get(pool_key)
           .map_or(Ok(()), |_| Err(InvariantError::PoolAlreadyExist))?;
 
-      self.pools.insert(pool_key, pool.clone());
+      self.pools.insert(pool_key.clone(), pool.clone());
       Ok(())
   }
 
-  pub fn update(&mut self, pool_key: PoolKey, pool: &Pool) -> Result<(), InvariantError> {
+  pub fn update(&mut self, pool_key: &PoolKey, pool: &Pool) -> Result<(), InvariantError> {
       self.get(pool_key)?;
 
-      self.pools.insert(pool_key, pool.clone());
+      self.pools.insert(pool_key.clone(), pool.clone());
       Ok(())
   }
 
   #[allow(dead_code)]
-  pub fn remove(&mut self, pool_key: PoolKey) -> Result<(), InvariantError> {
+  pub fn remove(&mut self, pool_key: &PoolKey) -> Result<(), InvariantError> {
       self.get(pool_key)?;
 
-      self.pools.remove(&pool_key);
+      self.pools.remove(pool_key);
       Ok(())
   }
 
-  pub fn get(&self, pool_key: PoolKey) -> Result<Pool, InvariantError> {
+  pub fn get(&self, pool_key: &PoolKey) -> Result<Pool, InvariantError> {
       let pool = self
           .pools
-          .get(&pool_key)
+          .get(pool_key)
           .cloned()
           .ok_or(InvariantError::PoolNotFound)?;
 
@@ -67,11 +67,11 @@ mod tests {
       let new_pool_key = PoolKey::new(token_x, token_y, new_fee_tier).unwrap();
       let pool = Pool::default();
 
-      pools.add(pool_key, &pool).unwrap();
-      assert_eq!(pools.get(pool_key), Ok(pool.clone()));
-      assert_eq!(pools.get(new_pool_key), Err(InvariantError::PoolNotFound));
+      pools.add(&pool_key, &pool).unwrap();
+      assert_eq!(pools.get(&pool_key), Ok(pool.clone()));
+      assert_eq!(pools.get(&new_pool_key), Err(InvariantError::PoolNotFound));
 
-      let result = pools.add(pool_key, &pool);
+      let result = pools.add(&pool_key, &pool);
       assert_eq!(result, Err(InvariantError::PoolAlreadyExist));
   }
 
@@ -96,12 +96,12 @@ mod tests {
           ..Pool::default()
       };
 
-      pools.add(pool_key, &pool).unwrap();
+      pools.add(&pool_key, &pool).unwrap();
 
-      pools.update(pool_key, &new_pool).unwrap();
-      assert_eq!(pools.get(pool_key), Ok(new_pool.clone()));
+      pools.update(&pool_key, &new_pool).unwrap();
+      assert_eq!(pools.get(&pool_key), Ok(new_pool.clone()));
 
-      let result = pools.update(new_pool_key, &new_pool);
+      let result = pools.update(&new_pool_key, &new_pool);
       assert_eq!(result, Err(InvariantError::PoolNotFound));
   }
 
@@ -117,12 +117,12 @@ mod tests {
       let pool_key = PoolKey::new(token_x, token_y, fee_tier).unwrap();
       let pool = Pool::default();
 
-      pools.add(pool_key, &pool).unwrap();
+      pools.add(&pool_key, &pool).unwrap();
 
-      pools.remove(pool_key).unwrap();
-      assert_eq!(pools.get(pool_key), Err(InvariantError::PoolNotFound));
+      pools.remove(&pool_key).unwrap();
+      assert_eq!(pools.get(&pool_key), Err(InvariantError::PoolNotFound));
 
-      let result = pools.remove(pool_key);
+      let result = pools.remove(&pool_key);
       assert_eq!(result, Err(InvariantError::PoolNotFound));
   }
 }
