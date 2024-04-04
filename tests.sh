0450c61596af6e1af29c64c967197578cfb6a0ce
none00y <mich_smyk@wp.pl>
2024-04-04 18:54:09 +0200

    add backtrace to CI

--- tests.sh
+++ tests.sh
@@ -5,5 +5,5 @@ cargo build -p fungible-token --release
 
 # Download node binary
 cargo xtask node
-# Run tests
-cargo test --workspace
+# Run tests with backtrace
+RUST_BACKTRACE=1 cargo test --workspace
