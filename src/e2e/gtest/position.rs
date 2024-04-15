3ff2068436191aa4fdddbfd4a2b134cfd31fcdf0
none00y <mich_smyk@wp.pl>
2024-04-15 17:33:36 +0200

    add remove position test

--- src/e2e/gtest/position.rs
+++ src/e2e/gtest/position.rs
@@ -6,8 +6,12 @@ use gstd::{prelude::*, ActorId};
 use gtest::*;
 use io::*;
 use math::{
-    fee_growth::FeeGrowth, liquidity::Liquidity, percentage::Percentage,
-    sqrt_price::calculate_sqrt_price, token_amount::TokenAmount,
+    fee_growth::FeeGrowth,
+    liquidity::Liquidity,
+    percentage::Percentage,
+    sqrt_price::{calculate_sqrt_price, SqrtPrice},
+    token_amount::TokenAmount,
+    MIN_SQRT_PRICE,
 };
 
 #[test]
@@ -905,4 +909,206 @@ fn test_create_position_insufficient_allowance_token_y() {
     assert_eq!(user_1_x, initial_balance);
     assert_eq!(user_1_y, initial_balance);
     assert_eq!(&pool_state, &pool_state_before);
+}
+
+#[test]
+fn test_remove_position() {
+    let sys = System::new();
+    sys.init_logger();
+
+    let token_x = ActorId::from(TOKEN_X_ID);
+    let token_y = ActorId::from(TOKEN_Y_ID);
+
+    let initial_mint = 10u128.pow(10);
+    let invariant = init_invariant(&sys, Percentage::from_scale(1, 2));
+    let (token_x_program, token_y_program) = init_tokens(&sys);
+
+    let fee_tier = FeeTier::new(Percentage::from_scale(6, 3), 10).unwrap();
+    let init_tick = 0;
+    let init_sqrt_price = calculate_sqrt_price(init_tick).unwrap();
+    let remove_position_index = 0;
+
+    let pool_key = PoolKey::new(token_x, token_y, fee_tier).unwrap();
+
+    invariant
+        .send(ADMIN, InvariantAction::AddFeeTier(fee_tier))
+        .assert_success();
+
+    invariant
+        .send(
+            REGULAR_USER_1,
+            InvariantAction::CreatePool {
+                token_0: token_x,
+                token_1: token_y,
+                fee_tier,
+                init_sqrt_price,
+                init_tick,
+            },
+        )
+        .assert_success();
+
+    let lower_tick_index = -20;
+    let upper_tick_index = 10;
+    let liquidity_delta = Liquidity::from_integer(1_000_000);
+
+    token_x_program
+        .send(REGULAR_USER_1, FTAction::Mint(initial_mint))
+        .assert_success();
+    token_y_program
+        .send(REGULAR_USER_1, FTAction::Mint(initial_mint))
+        .assert_success();
+
+    increase_allowance(&token_x_program, REGULAR_USER_1, INVARIANT_ID, initial_mint)
+        .assert_success();
+    increase_allowance(&token_y_program, REGULAR_USER_1, INVARIANT_ID, initial_mint)
+        .assert_success();
+
+    let pool_state = get_pool(&invariant, token_x, token_y, fee_tier).unwrap();
+
+    invariant
+        .send(
+            REGULAR_USER_1,
+            InvariantAction::CreatePosition {
+                pool_key,
+                lower_tick: lower_tick_index,
+                upper_tick: upper_tick_index,
+                liquidity_delta,
+                slippage_limit_lower: pool_state.sqrt_price,
+                slippage_limit_upper: pool_state.sqrt_price,
+            },
+        )
+        .assert_success();
+
+    let pool_state = get_pool(&invariant, token_x, token_y, fee_tier).unwrap();
+
+    assert_eq!(pool_state.liquidity, liquidity_delta);
+
+    let liquidity_delta = Liquidity::new(liquidity_delta.get() * 1_000_000);
+    let incorrect_lower_tick_index = lower_tick_index - 50;
+    let incorrect_upper_tick_index = upper_tick_index + 50;
+
+    increase_allowance(
+        &token_x_program,
+        REGULAR_USER_1,
+        INVARIANT_ID,
+        liquidity_delta.get(),
+    )
+    .assert_success();
+    increase_allowance(
+        &token_y_program,
+        REGULAR_USER_1,
+        INVARIANT_ID,
+        liquidity_delta.get(),
+    )
+    .assert_success();
+
+    invariant
+        .send(
+            REGULAR_USER_1,
+            InvariantAction::CreatePosition {
+                pool_key,
+                lower_tick: incorrect_lower_tick_index,
+                upper_tick: incorrect_upper_tick_index,
+                liquidity_delta,
+                slippage_limit_lower: pool_state.sqrt_price,
+                slippage_limit_upper: pool_state.sqrt_price,
+            },
+        )
+        .assert_success();
+
+    let position_state = get_position(&invariant, REGULAR_USER_1.into(), 1).unwrap();
+
+    // Check position
+    assert!(position_state.lower_tick_index == incorrect_lower_tick_index);
+    assert!(position_state.upper_tick_index == incorrect_upper_tick_index);
+
+    let amount = 1000;
+    token_x_program
+        .send(REGULAR_USER_2, FTAction::Mint(amount))
+        .assert_success();
+    assert_eq!(balance_of(&token_x_program, REGULAR_USER_2), amount);
+
+    increase_allowance(&token_x_program, REGULAR_USER_2, INVARIANT_ID, amount).assert_success();
+
+    let pool_state_before = get_pool(&invariant, token_x, token_y, fee_tier).unwrap();
+
+    let swap_amount = TokenAmount::new(amount);
+    let slippage = SqrtPrice::new(MIN_SQRT_PRICE);
+    invariant
+        .send(
+            REGULAR_USER_2,
+            InvariantAction::Swap {
+                pool_key,
+                x_to_y: true,
+                amount: swap_amount,
+                by_amount_in: true,
+                sqrt_price_limit: slippage,
+            },
+        )
+        .assert_success();
+
+    let pool_state_after = get_pool(&invariant, token_x, token_y, fee_tier).unwrap();
+    assert_eq!(
+        pool_state_after.fee_growth_global_x,
+        FeeGrowth::new(49999950000049999)
+    );
+    assert_eq!(pool_state_after.fee_protocol_token_x, TokenAmount(1));
+    assert_eq!(pool_state_after.fee_protocol_token_y, TokenAmount(0));
+
+    assert!(pool_state_after
+        .sqrt_price
+        .lt(&pool_state_before.sqrt_price));
+
+    assert_eq!(pool_state_after.liquidity, pool_state_before.liquidity);
+    assert_eq!(pool_state_after.current_tick_index, -10);
+    assert_ne!(pool_state_after.sqrt_price, pool_state_before.sqrt_price);
+
+    let amount_x = balance_of(&token_x_program, REGULAR_USER_2);
+    let amount_y = balance_of(&token_y_program, REGULAR_USER_2);
+    assert_eq!(amount_x, 0);
+    assert_eq!(amount_y, 993);
+
+    // pre load dex balances
+    let invariant_x_before_remove = balance_of(&token_x_program, INVARIANT_ID);
+    let invariant_y_before_remove = balance_of(&token_y_program, INVARIANT_ID);
+
+    // Remove position
+    invariant
+        .send(
+            REGULAR_USER_1,
+            InvariantAction::RemovePosition {
+                position_id: remove_position_index,
+            },
+        )
+        .assert_success();
+
+    // Load states
+    let pool_state = get_pool(&invariant, token_x, token_y, fee_tier).unwrap();
+    let lower_tick = get_tick(&invariant, pool_key, lower_tick_index);
+    let upper_tick = get_tick(&invariant, pool_key, lower_tick_index);
+    let lower_tick_bit = is_tick_initialized(&invariant, pool_key, lower_tick_index);
+    let upper_tick_bit = is_tick_initialized(&invariant, pool_key, upper_tick_index);
+    let dex_x = balance_of(&token_x_program, INVARIANT_ID);
+    let dex_y = balance_of(&token_y_program, INVARIANT_ID);
+    let expected_withdrawn_x = 499;
+    let expected_withdrawn_y = 999;
+    let expected_fee_x = 0;
+
+    assert_eq!(
+        invariant_x_before_remove - dex_x,
+        expected_withdrawn_x + expected_fee_x
+    );
+    assert_eq!(invariant_y_before_remove - dex_y, expected_withdrawn_y);
+
+    // Check ticks
+    assert_eq!(lower_tick, Err(InvariantError::TickNotFound));
+    assert_eq!(upper_tick, Err(InvariantError::TickNotFound));
+
+    // Check tickmap
+    assert!(!lower_tick_bit);
+    assert!(!upper_tick_bit);
+
+    // Check pool
+    assert!(pool_state.liquidity == liquidity_delta);
+    assert!(pool_state.current_tick_index == -10);
 }
