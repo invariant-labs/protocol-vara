029e4e75a4e487482a3158ea751e6a72555c8944
none00y <mich_smyk@wp.pl>
2024-06-05 17:22:40 +0200

    update CI tests to use release version

--- tests.sh
+++ tests.sh
@@ -2,4 +2,4 @@
 # Download node binary
 cargo xtask node
 # Run tests with backtrace
-RUST_BACKTRACE=1 cargo test --workspace --features gear-erc20/test
+RUST_BACKTRACE=1 cargo test --workspace --features gear-erc20/test $1
