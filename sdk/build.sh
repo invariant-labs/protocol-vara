f98fa7b5a9ede3e33d275f379980ae263936e847
none00y <mich_smyk@wp.pl>
2024-06-14 14:11:58 +0200

    fix CI

--- sdk/build.sh
+++ sdk/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-npm install
-npm run lint
-npm run wasm:build
-npm run contract:build
-npm run erc-20:copy
-npm run erc-20:generate
+npm install &&
+npm run lint &&
+npm run wasm:build &&
+# second install for wasm to be added
+npm install &&
+npm run contract:build &&
+npm run erc-20:generate &&
 npm run build
